9e9afa24dbf6d6f3408d05506af73d03eefcb806
AfonsoJ3 <116575579+AfonsoJ3@users.noreply.github.com>
2025-02-16 22:17:24 -0500

    Delete Assets directory

--- Assets/Scripts/MenuManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class MenuManager : MonoBehaviour
-{
-    public void onPlayClick()
-    {
-        SceneManager.LoadScene("MainGame");
-    }
-}
